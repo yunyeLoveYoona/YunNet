a676d115bfbe0673bec27db65cce107c9f8b1fb9
yunyeLoveYoona <550752356@qq.com>
2015-07-30 10:22:37 +0800

    修改请求结果进行json转化时的错误，对JsonRequest对象实例化新增TypeToken参数用于请求结果的json转化

--- app/src/main/java/org/yun/net/core/requests/JsonRequest.java
+++ app/src/main/java/org/yun/net/core/requests/JsonRequest.java
@@ -6,56 +6,55 @@ import com.google.gson.reflect.TypeToken;
 import org.apache.http.NameValuePair;
 import org.yun.net.core.Request;
 import org.yun.net.core.http.YunHttp;
-
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by yune on 15-6-12.
  */
 public class JsonRequest<T> extends Request{
-    public JsonRequest(int priority, String url, int tag, RequestListenter<T> requestListenter) {
+    private TypeToken<T> typeToken;
+    public JsonRequest(int priority, String url, int tag,
+                       RequestListenter<T> requestListenter, TypeToken<T> typeToken) {
         super(priority, url, tag, requestListenter);
+        this.typeToken = typeToken;
     }
-
-    public JsonRequest(int priority, String url, List<NameValuePair> nameValuePairList,
-                       int tag, RequestListenter<T> requestListenter) {
+    public JsonRequest(int priority, String url,
+                       List<NameValuePair> nameValuePairList, int tag,
+                       RequestListenter<T> requestListenter, TypeToken<T> typeToken) {
         super(priority, url, nameValuePairList, tag, requestListenter);
+        this.typeToken = typeToken;
     }
-
     @Override
     protected void startRequest() {
-        if(httpHeadMap==null){
+        if (httpHeadMap == null) {
             setDefaultHttpHead();
         }
         YunHttp yunHttp = getYunHttp();
         try {
-        String resultstr;
-        if(nameValuePairList != null&&nameValuePairList.size()>0){
-                resultstr = yunHttp.poet(url,nameValuePairList);
-        }else{
-            resultstr = yunHttp.get(url);
-        }
+            String resultstr;
+            if (nameValuePairList != null && nameValuePairList.size() > 0) {
+                resultstr = yunHttp.poet(url, nameValuePairList);
+            } else {
+                resultstr = yunHttp.get(url);
+            }
             forResult(resultstr);
         } catch (Exception e) {
-            if(requestListenter != null){
-                requestListenter.onError(e,tag);
+            if (requestListenter != null) {
+                requestListenter.onError(e, tag);
             }
         }
     }
-
     @Override
-    protected void forResult(String resultStr) throws Exception{
-            Gson gson = new Gson();
-            final T result = gson.fromJson(resultStr,new TypeToken<T>(){}.getType());
-            if(requestListenter != null){
-                mainLooperHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        requestListenter.onComlete(result, tag);
-                    }
-                });
-            }
+    protected void forResult(String resultStr) throws Exception {
+        Gson gson = new Gson();
+        final T result = gson.fromJson(resultStr, typeToken.getType());
+        if (requestListenter != null) {
+            mainLooperHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    requestListenter.onComlete(result, tag);
+                }
+            });
+        }
     }
 }
